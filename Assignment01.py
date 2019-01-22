a5dd7f5a6cc7e593a6e3360184c17b6e5a12764b
jillmead <35549254+jillmead@users.noreply.github.com>
2019-01-21 22:26:53 -0600

    Add files via upload

---
+++ Assignment01.py
@@ -0,0 +1,14 @@
+x = 4
+y = 7
+z = x + y
+print(z)
+
+noun = "Apples"
+verb = " are"
+adjective = " red, yellow, and green!"
+sentence = noun + verb + adjective
+print(sentence)
+
+print(sentence + " contains " + str(len(sentence)) + " characters.")
+
+path = r"C:\Users\jill\Dropbox\GIS Programming"
